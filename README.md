4be429e7c4120cf50158e01b42f6414456d2d57d
Ken Youens-Clark <kyclark@gmail.com>
2025-10-24 14:44:34 -0400

    formatting, added KEGG URL

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mbryg
+
 Command line tool for retrieving metabolic information from Human Genome-scale metabolic model
--- mbryg.py
+++ mbryg.py
@@ -6,40 +6,42 @@ Purpose: Rock the Casbah
 """
 
 import argparse
-import requests 
+import requests
 import sys
 import csv
 from pprint import pprint
 
-API_URL = 'https://www.vmh.life/_api/metabolites/?search={}'
+API_URL = "https://www.vmh.life/_api/metabolites/?search={}"
 
 
 # --------------------------------------------------
 def get_args():
-    """ Get command-line arguments """
+    """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Rock the Casbah",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('metabolite_name',
-                        metavar='ID',
-                        help='Metablite name')
+    parser.add_argument(
+        "metabolite_name", metavar="ID", help="Metablite name"
+    )
 
-    parser.add_argument('-o',
-                        '--outfile',
-                        help='The output file',
-                        metavar='FILE',
-                        type=argparse.FileType('wt'),
-                        default=sys.stdout)
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="The output file",
+        metavar="FILE",
+        type=argparse.FileType("wt"),
+        default=sys.stdout,
+    )
 
     return parser.parse_args()
 
- 
 
 # --------------------------------------------------
 def main() -> None:
-    """ Make a jazz noise here """
+    """Make a jazz noise here"""
 
     args = get_args()
     print(args.metabolite_name)
@@ -47,26 +49,27 @@ def main() -> None:
     url = API_URL.format(name)
     req = requests.get(url)
     if req.status_code != 200:
-        sys.exit(f'Failed to get {url}')
-    #print(req.json())
+        sys.exit(f"Failed to get {url}")
+    # print(req.json())
     data = req.json()
-    #pprint(data)
-    if data['count'] == 0:
+    # pprint(data)
+    if data["count"] == 0:
         sys.exit(f'Unable to find "{name}"')
 
     results = data["results"]
     while True:
         if next_url := data["next"]:
-            #print(next_url)
+            # print(next_url)
             req = requests.get(next_url)
             if req.status_code != 200:
-                sys.exit(f'Failed to get {next_url}')
+                sys.exit(f"Failed to get {next_url}")
             data = req.json()
-            #pprint(data)
+            # pprint(data)
             results += data["results"]
         else:
             break
-    #print(results, file=args.outfile)
+    # https://rest.kegg.jp/list/
+    # print(results, file=args.outfile)
     # for i, result in enumerate(results):
     #     if i == 0:
     #         print(",".join(result.keys()), file=args.outfile)
@@ -77,6 +80,7 @@ def main() -> None:
     for result in results:
         writer.writerow(result)
 
+
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
