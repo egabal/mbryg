25810eb81e8805b9ee6d99aa74810da24455e55b
Ken Youens-Clark <kyclark@gmail.com>
2025-10-25 17:06:43 -0400

    python script to run smiles2png.py

---
+++ mk_smiles.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Author : Ken Youens-Clark <kyclark@gmail.com>
+Date   : 2025-10-25
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import csv
+import os
+from typing import NamedTuple, TextIO
+
+from pprint import pprint
+
+
+class Args(NamedTuple):
+    """ Command-line arguments """
+    file: TextIO
+    outdir: str
+
+
+# --------------------------------------------------
+def get_args() -> Args:
+    """ Get command-line arguments """
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        metavar='STR',
+                        type=argparse.FileType("rt"),
+                        help='SMILE string')
+
+    parser.add_argument('-o',
+                        '--outdir',
+                        help='Ouput dir',
+                        metavar='DIR',
+                        default='smiles')
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.outdir):
+        os.makedirs(args.outdir)
+
+    return Args(file=args.file, outdir=args.outdir)
+
+
+# --------------------------------------------------
+def main() -> None:
+    """ Make a jazz noise here """
+
+    args = get_args()
+    reader = csv.DictReader(args.file)
+
+    for rec in reader:
+        #pprint(rec)
+        print("./smiles2png.py '{}' -o {}".format(
+            rec["smile"],
+            os.path.join(args.outdir, rec["abbreviation"] + ".png")
+        ))
+        break
+
+    print(f"Done, see \"{args.outdir}\"")
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
