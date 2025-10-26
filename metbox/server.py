d59b7a078b9e296b52c40391f45bd41a006668e0
Ken Youens-Clark <kyclark@gmail.com>
2025-10-25 22:06:12 -0400

    moved images to static directory for flask

--- metbox/server.py
+++ metbox/server.py
@@ -2,35 +2,87 @@ from flask import Flask, url_for
 import csv
 
 app = Flask(__name__)
-DB_FILE = 'human_mets.csv'
+DB_FILE = "db.csv"
+
 
 @app.route("/")
 def metabolite_list():
     with open(DB_FILE) as csvfile:
         reader = csv.DictReader(csvfile)
-        html = '<link rel="stylesheet" href="{}">'.format(url_for('static', filename='styles.css'))
+        html = '<link rel="stylesheet" href="{}">'.format(
+            url_for("static", filename="styles.css")
+        )
         html += '<main class="container"><h1>Metabolite List</h1><ul class="compound-list">'
-        compounds_seen = dict()
+        metabolites = dict()
         for row in reader:
-            compound_id = row["Compound_ID"]
-            full_name = row.get("fullName", "Unknown name")
-            if compound_id not in compounds_seen:
-                compounds_seen[compound_id] = full_name
+            name = row.get("fullName", "Unknown name")
+            metabolites[name] = row
 
-    
-        for compound_id in sorted(compounds_seen):
-            full_name = compounds_seen[compound_id]
-            html += f'<li><a href="/compound/{compound_id}">{full_name} = {compound_id}</a></li>'
-        html += '</ul></main>'
+        for name in sorted(metabolites):
+            rec = metabolites[name]
+            abbr = rec["abbreviation"]
+            # html += f'<li><a href="/compound/{compound_id}">{full_name} = {compound_id}</a></li>'
+            html += f'<li><a href="/metabolite/{abbr}">{name}</a></li>'
+        html += "</ul></main>"
         return html
 
 
-@app.route('/compound/<compound_id>')
+@app.route("/metabolite/<name>")
+def show_metabolite(name):
+    with open(DB_FILE) as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        html = '<link rel="stylesheet" href="{}">'.format(
+            url_for("static", filename="styles.css")
+        )
+        html += '<main class="container">'
+        full_name = ""
+        description = ""
+        pathways = set()
+
+        record = None
+        for row in reader:
+            if row["abbreviation"] == name:
+                record = row
+
+        if record:
+            pathway_ids = record.get("Pathway_ID").split(";")
+            pathway_names = record.get("Pathway_Name").split(";")
+            pathways = zip(pathway_names, pathway_ids)
+
+            full_name = record.get("fullName", "Unknown chemical")
+            description = record.get(
+                "description", "No description available"
+            )
+            # pathways.add((row["Pathway_ID"], row["Pathway_Name"]))
+            html += f"<h1>{full_name}</h1>"
+            html += f"<p><strong>Metabolite:</strong> {name}</p>"
+            html += f"<p><strong>Description:</strong> {description}</p>"
+
+            html += "<h2>Associated Pathways</h2>"
+            html += "<table class='data-table'><tr><th>Pathway ID</th><th>Pathway Name</th></tr>"
+            for pid, pname in sorted(pathways):
+                html += f"<tr><td>{pid}</td><td>{pname}</td></tr>"
+            html += "</table>"
+
+            if row["smile"]:
+                html += "<img src='/static/images/" + record["abbreviation"] + ".png'>"
+
+            html += "<a class='back' href='/'>← Back to list</a></main>"
+            return html
+
+        else:
+            return f"<main class='container'><h1>Compound {compound_id} not found</h1><a class='back' href='/'>← Back</a></main>"
+
+
+@app.route("/compound/<compound_id>")
 def show_compound(compound_id):
     with open(DB_FILE) as csvfile:
         reader = csv.DictReader(csvfile)
 
-        html = '<link rel="stylesheet" href="{}">'.format(url_for('static', filename='styles.css'))
+        html = '<link rel="stylesheet" href="{}">'.format(
+            url_for("static", filename="styles.css")
+        )
         html += '<main class="container">'
         full_name = ""
         description = ""
@@ -38,10 +90,12 @@ def show_compound(compound_id):
 
         for row in reader:
             print(row)
-            if row['Compound_ID'] == compound_id:
+            if row["Compound_ID"] == compound_id:
                 if not full_name:
                     full_name = row.get("fullName", "Unknown chemical")
-                    description = row.get("description", "No description available")
+                    description = row.get(
+                        "description", "No description available"
+                    )
                 pathways.add((row["Pathway_ID"], row["Pathway_Name"]))
 
         if full_name:
